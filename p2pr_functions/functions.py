d1130dae1c83e3cbc97c3d62d641527e0e633f04
Ben McCarty <63123859+BenJMcCarty@users.noreply.github.com>
2021-07-14 13:33:45 -0400

    Updated feature engineering

--- p2pr_functions/functions.py
+++ p2pr_functions/functions.py
@@ -13,8 +13,18 @@ def find_outliers_z(data):
     Returns a boolean Series where True=outlier
     
     Source: https://github.com/jirvingphd/dsc-phase-2-project/blob/main/functions_SG.py
+
+    Args:
+        data (pd.Series): Series for which to determine the outliers via the Z-score
+
+    Returns:
+        pd.Series: Boolean index indicating "True" if a value is an outlier
     """
-    
+    import numpy as np
+    import scipy as stats
+    import matplotlib.pyplot as plt
+    import pandas as pd
+
     zFP = np.abs(stats.zscore(data))
     zFP = pd.Series(zFP, index=data.index)
     idx_outliers = zFP > 3
@@ -24,14 +34,17 @@ def find_outliers_z(data):
 ## Check using IQR - less sensitive to outliers
 
 def find_outliers_IQR(data):
-    """
-    * Takes a series sliced from a dataframe
-    * Detects outliers using the 1.5*IQR thresholds.
-    * Returns a boolean Series where True=outlier
+    """Determines outliers using the 1.5*IQR thresholds.
 
     Source: https://github.com/jirvingphd/dsc-phase-2-project/blob/main/functions_SG.py
-    """
-    
+
+    Args:
+        data (pd.Series): [description]
+
+    Returns:
+        pd.Series: Boolean Series where True=outlier
+    """    
+        
     res = data.describe()
     q1 = res['25%']
     q3 = res['75%']
@@ -40,38 +53,35 @@ def find_outliers_IQR(data):
     return idx_outliers
 
 ###
-def feature_vis(data, x, y = 'price', discrete = False, kde = True):
-    '''-----
-    * Requires a DataFrame and a column name to process.
-    * Keyword arguments specify that the target variable will be "price"
-    for this case.
-    * For future use, redefine function without predetermined y-value, or 
-    reassign.
+def feature_vis(data, x, y = 'price', categorical = False, kde = True):
+    """
+    Prints the selected Series for reference.
     
-    --
+    Creates two plots via Seaborn:
+
+        * Scatter plot with regression line
+        * Histogram of the data
+            * Optional KDE ifkde = True
+
+    Args:
+        data (pd.DataFrame): Source dataframe
+        x (pd.Series): Independent variable for visualizations
+        y (str, optional): Dependent variable. Defaults to 'price'.
+        categorical (bool, optional): Indicate if the values are categorical. Defaults to False.
+        kde (bool, optional): Add KDE plot to histogram. Defaults to True.
     
-    * Args:
-        * Data: Pandas DataFrame; data source
-        * x: str; column index to specify data
-    
-    * Kwargs:
-        * y = "price"
-        * discrete = False
-        * kde = true
-        
-    -----'''
-    
-    ## Print the slice of the original DataFrame for easy viewing
-    
-    print(df[x].value_counts().sort_index())
-  
-    ## Create two plots via Seaborn: one scatter plot with regression line,
-    ## then a histogram of the data (with KDE if specified
-    
+    Returns:
+        N/a
+    """
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    print(data[x].value_counts().sort_index())
+      
     fig, axs = plt.subplots(ncols=2, figsize= (12,6))
     
     sns.regplot(data=data, x=x, y=y, ax=axs[0])
-    sns.histplot(data=data, x=x, discrete=discrete, kde=kde, ax=axs[1])
+    sns.histplot(data=data, x=x, discrete=categorical, kde=kde, ax=axs[1])
     
     fig.suptitle(f'{x.title()} vs. {y.title()}', fontsize=16)
     plt.tight_layout();
@@ -80,72 +90,59 @@ def feature_vis(data, x, y = 'price', discrete = False, kde = True):
 
 ###
 def filter_outliers(data):
-    '''------
-    
-    * Removes outliers from data via "find_outliers_IQR" and saves filtered
-    values to the dataframe
-    
-    ---
-    
-    * Arg:
-        * Data: slice of a dataframe for a specific column header
-    
-    ------
-    '''
-   
+    """Filters outliers from given data via the "find_outliers_IQR" function and saves filtered
+    values to a new DataFrame
+
+    Args:
+        data (pd.Series): Selected Series
+
+    Returns:
+        pd.DataFrame: DataFrame of non-outlier data filtered from original DataFrame
+    """    
+       
     idx_out = find_outliers_IQR(data)
  
-    cleaned = df[~idx_out]
+    cleaned = data[~idx_out].copy()
 
     print(f'There were {idx_out.sum()} outliers.')
     
     return cleaned
 
 ###
-def remove_outliers(data, x):
-
-    idx_out = find_outliers_IQR(data[x])
- 
-    df_clean = df[~idx_out].copy()
-    
-    return df_clean
-
-###
-def show_cleaned_vis(data, x, y = 'price', discrete = False, kde = True):
-    '''-----
-    
-    * Combines functions to filter outliers and to create the feature 
+def show_cleaned_vis(data, x, y = 'price', categorical = False, kde = True):
+    """Combines helper functions to filter outliers and to create the feature 
         visualizations.
-    * Requres 'find_outliers_IQR' and 'feature_vis' to be defined.
-    * Returns filtered data and two visualizations - Seaborn regression plot
-        and histplot.
     
-    ---
+    * Requres 'find_outliers_IQR' and 'feature_vis' to be pre-defined
+
+    Args:
+        data (pd.DataFrame): Source data
+        x (str): Independent variable to visualize
+        y (str, optional): Dependent variable against which to plot the independent variable. Defaults to 'price'.
+        categorical (bool, optional): Indicates whether or not 'x' is categorical. Defaults to False.
+        kde (bool, optional): Overlay a KDE plot. Defaults to True.
+
+    Returns:
+        None
+    """
     
-    * Args:
-        * Data: Pandas DataFrame; data source
-        * x: str; column index to specify data
-    * Kwargs
-    
-    -----'''
-    
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
     ### Filter outliers first
     
     idx_out = find_outliers_IQR(data[x])
  
-    df_cleaned = df[~idx_out].copy()
+    df_cleaned = data[~idx_out].copy()
 
-    print(f'There were {idx_out.sum()} outliers.')
-    
     ### Plot Data
-    
-    
-    df_cleaned[x].value_counts().sort_index()
+        
+    df_cleaned.value_counts().sort_index()
         
     fig, axs = plt.subplots(ncols=2, figsize= (12,6))
     
     sns.regplot(data=df_cleaned, x=x, y=y, ax=axs[0],line_kws={"color": "red"})
-    sns.histplot(data=df_cleaned, x=x, discrete=discrete, kde=kde, ax=axs[1])
+    sns.histplot(data=df_cleaned, x=x, discrete=categorical, kde=kde, ax=axs[1])
     
     fig.suptitle(f'{x.title()} vs. {y.title()}', fontsize=16)
     plt.tight_layout();
@@ -154,37 +151,48 @@ def show_cleaned_vis(data, x, y = 'price', discrete = False, kde = True):
 
 ###
 def ttest_review(sample_1, sample_2, alpha=.05):
-    '''------
-    * Runs a t-test on two samples; prints whether or not they are significant,
+    """Runs a t-test on two samples from the same independent variable; prints whether or not they are significant;
     and returns p-value as a variable called "p-value."
-    * Requires two data samples and an alpha value.
-    
-    ----
-    
-    * Args: two data samples for t-test
-    * Kwargs: alpha=.05
-    
-    -----
-    '''
-    
+
+    Args:
+        sample_1 (pd.Series): First sample of a Series from source DataFrame to evaluate
+        sample_2 (pd.Series): Second sample of a Series from source DataFrame to evaluate
+        alpha (float, optional): Significance level for test. Defaults to .05.
+
+    Returns:
+        int: Resulting p-value for reference
+    """    
+    import scipy as stats
+
     result = stats.ttest_ind(sample_1, sample_2)
     crit_val, p_val = result
     
     ## Creating interpretation based on p-value results.
 
-    if p_value < .05:
-        print(f'The feature "waterfront" is statistically significant with a p-value of {p_val}.')
+    if p_val < .05:
+        print(f'The feature is statistically significant with a p-value of {p_val}.')
 
     else:
-         print(f'The feature "waterfront" is not statistically significant with a p-value of {p_val}.')
+         print(f'The feature is not statistically significant with a p-value of {p_val}.')
     
     return p_val
 
 ###
-# Create "corr_val" function
-
 def corr_val(df,figsize=(15,15),cmap="OrRd",):
-    
+    """Generates a Seaborn heatmap of correlations between each independent variable.
+
+    Args:
+        df (pd.Dataframe): Source DataFrame
+        figsize (tuple, optional): Size of resulting figure. Defaults to (15,15).
+        cmap (str, optional): Color scheme. Defaults to "OrRd".
+
+    Returns:
+        fig, ax: resulting visualization
+    """
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
     # Calculate correlations
     corr = df.corr()
        
@@ -199,38 +207,6 @@ def corr_val(df,figsize=(15,15),cmap="OrRd",):
     # Mask=mask to hide the upper-right half of values (otherwise mirrored)
     sns.heatmap(corr, annot=True,cmap="Reds",mask=mask)
     return fig, ax
-
-###
-def create_model(data, cont, cat):
-    '''------
-    
-    * Creates a linear regression model using Statsmodels OLS
-    * Save the 
-    
-    ---
-    
-    * Args:
-        * Data: dataframe as data source
-        * Cont: list of continuous variables
-        * Cat: list of categorical variables
-        * unique_id: UNIQUE name for the resulting model
-    
-    -----
-    '''
-
-    cont_features = '+'.join(cont)
-
-    cat_features = '+'.join([f'C({x})' for x in cat])
-
-    f = f'price~+{cont_features}+{cat_features}'
-
-    print(f)
-
-    model = smf.ols(formula=f, data=data).fit()
-   
-    diagnose_model(model)
-    
-    return model
 
 ###
 def diagnose_model(model, figsize=(10,5)):
@@ -250,7 +226,11 @@ def diagnose_model(model, figsize=(10,5)):
     ---
     
     """
-    display(model.summary())
+    import matplotlib.pyplot as plt
+    import statsmodels.formula.api as smf
+    import statsmodels.api as sms
+
+    print(model.summary())
     
     fig, axes = plt.subplots(ncols=2, figsize=figsize)
 
@@ -267,24 +247,46 @@ def diagnose_model(model, figsize=(10,5)):
     return
 
 ###
-def plot_param_coef(model, kind = 'barh', figsize = (10,5)):
-    ''' ---
-    
-    * Plotting a figure to visualize parameter coefficients
-    
-    ---
-    
-    * Args:
-        * Model: linear regression model details to plot
-        
-    * Kwargs:
-        * Kind (default 'barh'): allows different types of plots
-        * Size (default (10,10)): allows for different sizes
-    ---
-    
-    '''
-    # Plotting figure to visualize parameter coefficients
+def create_model(data, cont, cat, target):
+    """Creates a linear regression model using Statsmodels OLS and 
+    evaluates assumptions of linearity by plotting residuals for homoscedasticity
+    and a Q-Q plot for normality.
 
+    Args:
+        data (pd.DataFrame): Source DataFrame
+        cont (list): List of strings indicating which column names to treat as continuous data
+        cat (list): List of strings indicating which column names to treat as categorical data
+
+    Returns:
+        model: Statsmodels OLS Linear Regression model
+    """    
+    import statsmodels.formula.api as smf
+
+    cont_features = '+'.join(cont)
+
+    cat_features = '+'.join([f'C({x})' for x in cat])
+
+    f = f'{target}~+{cont_features}+{cat_features}'
+
+    print(f)
+
+    model = smf.ols(formula=f, data=data).fit()
+   
+    diagnose_model(model)
+    
+    return model
+
+###
+def plot_param_coef(model, kind = 'barh', figsize = (10,5)):
+    """Plotting a figure to visualize parameter coefficients
+
+    Args:
+        model (Statsmodels OLS model object): linear regression model details to plot
+        kind (str, optional): Plot type. Defaults to 'barh'.
+        figsize (tuple, optional): Figure size. Defaults to (10,5).
+    """
+    import matplotlib.pyplot as plt
+ 
     ## Getting coefficients as a Series
     params = model.params[1:]
     params.sort_values(inplace=True)
@@ -301,42 +303,38 @@ def plot_param_coef(model, kind = 'barh', figsize = (10,5)):
     return
 
 ###
-def plot_p_values(model, kind = 'barh', size = None, alpha = .05):
-    ''' ---
+def plot_p_values(model, kind = 'barh', figsize = (10,5), alpha = .05):
+    """Plots a figure to visualize parameter p-values exceeding stated alpha.
+
+    Args:
+        model (Statsmodels OLS model object): Model details to plot.
+        kind (str, optional): Plot type. Defaults to 'barh'.
+        figsize (tuple, optional): Figure size. Defaults to (10,5).
+        alpha (float, optional): Significance level (p-value). Defaults to .05.
+    """   
     
-    * Plots a figure to visualize parameter p-values exceeding stated alpha.
-    
-    ---
-    
-    * Args:
-        * Model: linear regression model details to plot
-        
-    * Kwargs:
-        * Kind (default 'barh'): allows different types of plots
-        * Size (default None): allows for different sizes
-    ---
-    
-    '''
-    
+    import matplotlib.pyplot as plt
+
     pv = model.pvalues[1:]
     pv_high = pv[pv > alpha]
     pv_low = pv[pv <= alpha]
     pv_high.sort_values(ascending=False, inplace=True)
     
     if len(pv_high) > 0:
-        plt.figure(figsize=size) # Used if large number of params
+        plt.figure(figsize=figsize) # Used if large number of params
         ax = pv_high.plot(kind=kind)
         ax = pv_low.plot(kind=kind)
         ax.axvline()
         plt.suptitle(f'P-Values')
         
     if len(pv_low) > 0:
-        plt.figure(figsize=size) # Used if large number of params
+        plt.figure(figsize=figsize) # Used if large number of params
         ax = pv_low.plot(kind=kind)
         ax.axvline()
         plt.suptitle(f'P-Values Below {alpha}')        
-        
-#     else:
+
+    ## Not used; keeping just in case        
+#   else:
 #         print(f'There are no p-values above {alpha}.')
         
     plt.tight_layout()
@@ -345,17 +343,12 @@ def plot_p_values(model, kind = 'barh', size = None, alpha = .05):
 
 ###
 def review_model(model):
-    '''---
-    
-    Combines earlier functions into one all-purpose function for reviewing
+    """Combines earlier functions into one all-purpose function for reviewing
     model performance.
-    
-    ---
-    
-    Arg:
-        * model: Specify model to review.
-        
-    ---'''
+
+    Args:
+        model (Statsmodels OLS model object): Model details to plot.
+    """    
     
     diagnose_model(model)
     
